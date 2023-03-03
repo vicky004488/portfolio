c25531e8bb032a15333b121b0a32671bb291b816
vicky-singh <126858648+vicky004488@users.noreply.github.com>
2023-03-03 13:40:43 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+  Strings: ["vicky-singh", "youtuber", "blogger", "kkkssjs"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
